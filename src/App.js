5a1ce58f9d1d1970f639a70e5dd51a1cb2b56c5c
Diogo Ortiz Machado <diogomachadors@gmail.com>
2019-07-10 18:49:07 -0300

    Fixed Playlist no working corretly

--- src/App.js
+++ src/App.js
@@ -61,9 +61,11 @@ class App extends Component {
     let parsed = querystring.parse(window.location.search);
     let accessToken = parsed['?access_token'];
 
-    // fetch('https://api.spotify.com/v1/me', { headers: { Authorization: 'Bearer ' + accessToken } })
-    //   .then(response => response.json())
-    //   .then(data => this.setState({ serverData: { user: { name: data.display_name } } }));
+    if (!accessToken) return;
+
+    fetch('https://api.spotify.com/v1/me', { headers: { Authorization: 'Bearer ' + accessToken } })
+      .then(response => response.json())
+      .then(data => this.setState({ user: { name: data.display_name } }));
 
     fetch('https://api.spotify.com/v1/me/playlists', {
       headers: { Authorization: 'Bearer ' + accessToken }
@@ -71,30 +73,30 @@ class App extends Component {
       .then(response => response.json())
       .then(data =>
         this.setState({
-          serverData: {
-            user: {
-              playlists: data.items.map(item => ({
-                name: item.name,
-                songs: []
-              }))
-            }
-          }
+          playlists: data.items.map(item => {
+            console.log(data.items);
+            return {
+              name: item.name,
+              imageUrl: item.images[0].url,
+              songs: []
+            };
+          })
         })
       );
   }
 
   render() {
     let playlistsToRender =
-      this.state.serverData.user && this.state.serverData.user.playlists
-        ? this.state.serverData.user.playlists.filter(playlist =>
+      this.state.user && this.state.playlists
+        ? this.state.playlists.filter(playlist =>
             playlist.name.toLowerCase().includes(this.state.filterString.toLowerCase())
           )
         : [];
     return (
       <div className="App">
-        {this.state.serverData.user ? (
+        {this.state.user ? (
           <div>
-            <h1>{this.state.serverData.user && this.state.serverData.user.name}'s Playlist</h1>
+            <h1>{this.state.user.name}'s Playlist</h1>
             <PlaylistCounter playlists={playlistsToRender} />
             <HoursCounter playlists={playlistsToRender} />
             <Filter onTextChange={text => this.setState({ filterString: text })} />
@@ -104,7 +106,11 @@ class App extends Component {
           </div>
         ) : (
           <button
-            onClick={() => (window.location = 'http://localhost:8888/login')}
+            onClick={() => {
+              window.location = window.location.href.includes('localhost')
+                ? 'http://localhost:8888/login'
+                : 'https://diogo-spotlists-backend.herokuapp.com/login';
+            }}
             style={{ padding: '20px', fontSize: '50px', marginTop: '20px' }}
           >
             Sign in with Spotify
--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -5,12 +5,12 @@ export default class Playlist extends Component {
     const playlist = this.props.playlist;
     return (
       <div style={{ width: '25%', display: 'inline-block' }}>
-        <img src="" alt="" />
+        <img src={playlist.imageUrl} style={{ width: '60px' }} alt="" />
         <h3>{playlist.name}</h3>
         <ul>
-          {playlist.songs.map((song, index) => {
-            return <li key={index}>{song.name}</li>;
-          })}
+          {playlist.songs.map((song, index) => (
+            <li key={index}>{song.name}</li>
+          ))}
         </ul>
       </div>
     );
